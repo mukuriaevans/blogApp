3535debfa203969878b512696a6a86a2b595734e
mukuriaevans <33530066+mukuriaevans@users.noreply.github.com>
2022-11-25 12:30:49 +0300

    Add process.env to port variable

--- app.js
+++ app.js
@@ -110,8 +110,12 @@ app.get('/delete', function(req, res){
   
 });
 
+let port = process.env.PORT;
+if (port == null || port == ""){
+  port = 3000;
+}
 
-app.listen(3000, function() {
+app.listen(port, function() {
   console.log("Server started on port 3000");
 });
 
